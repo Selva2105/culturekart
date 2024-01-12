748f514cddd3790a9d0887bc7ebf5c778f158e0d
Selvaganapathi_Kanakaraj <selvaganapathikanakaraj2105@gmail.com>
2024-01-13 00:11:56 +0530

    email updates

--- view/email-template.js
+++ view/email-template.js
@@ -69,7 +69,7 @@ const emailTemplate = (url, name) => {
         <p>Welcome to our exclusive community! We are honored to have you as a part of our journey.</p>
         <p>As you embark on this experience, we want to express our gratitude for choosing IKart.</p>
         <div class="link">
-            <a href="#" id="verificationLink" class="button" target="_blank">Click to verify</a>
+        <a href="${url}" id="verificationLink" class="button" target="_blank">Click to verify</a>        
         </div>
         <p class="link-p">Click the above link to verify your account</p>
         <p>Feel free to explore our offerings, and if you have any questions or need assistance, don't hesitate to reach out.</p>
@@ -80,7 +80,6 @@ const emailTemplate = (url, name) => {
     <script>
         document.getElementById('verificationLink').addEventListener('click', function(event) {
             event.preventDefault();
-            // Make a POST request when the link is clicked
             fetch('${url}', {
                 method: 'POST',
                 headers: {
@@ -89,7 +88,6 @@ const emailTemplate = (url, name) => {
                 body: JSON.stringify({}),
             })
             .then(response => {
-                // Handle the response as needed
                 console.log('POST request successful', response);
             })
             .catch(error => {
